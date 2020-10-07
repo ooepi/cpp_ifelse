d940cdf4b6c22a0e914cd5a7c48c88a1831b35c2
ooepi <robert.harmaakivi@gmail.com>
2020-10-07 11:13:46 +0300

    Finished first program. This will ask the user for two numbers, and compare them

--- ifelse.cpp
+++ ifelse.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main(){
 
-    cout << "Hello world";
+    int first;
+    int second;
+
+    cout << "Give two integers \n";
+
+    cout << "First: ";
+    cin >> first;
+
+    cout << "Second: ";
+    cin >> second;
+
+    if(first == 10 * second){
+        cout << "You gave a large first number";
+    } else {
+        cout << "the numbers are approximately the same";
+    }
+
     return 0;
 }
